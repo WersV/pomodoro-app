e76c4a42abcc7265b8776951f3e45f68bb0b9322
Przemyslaw Botwina <przemek.botwina@o2.pl>
2023-09-18 15:53:04 +0200

    add input component

---
+++ src/components/AppInput.vue
@@ -0,0 +1,36 @@
+<template>
+  <label for="input" v-if="label">{{ label }}</label>
+  <input 
+    v-bind="$attrs"
+    :value="modelValue"
+    @input="$emit('update:modelValue', $event.target.value)"
+    :placeholder="label"
+    class="label"
+    id="input"
+  >
+
+  <p
+    v-if="error"
+    class="errorMessage">
+    {{ error }}
+  </p>
+</template>
+<script setup>
+defineProps({
+  label: {
+    type: String,
+    default: ''
+  },
+  modelValue: {
+    type: [String, Number],
+    default: ''
+  },
+  error: {
+
+  }
+})
+defineEmits(['update:modelValue'])
+</script>
+<style lang="scss" scoped>
+
+</style>
