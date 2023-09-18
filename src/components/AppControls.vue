46ef343c5ff35b0687d5831e437329f1ba7a3548
Przemyslaw Botwina <przemek.botwina@o2.pl>
2023-09-18 16:19:41 +0200

    add control btns

---
+++ src/components/AppControls.vue
@@ -0,0 +1,19 @@
+<template>
+  <button @click="$emit('handleCountdown')">{{ flag ? 'Stop' : 'Start' }}</button>
+  <button @click="$emit('resetCountdown')">Reset</button>
+  <button @click="$emit('handleConfiguration')">Config</button>
+</template>
+
+<script setup>
+
+defineProps({
+  flag: {
+    type: Boolean,
+    default: false
+  }
+})
+
+defineEmits(['handleCountdown', 'resetCountdown', 'handleConfiguration'])
+</script>
+
+<style lang="scss" scoped></style>
