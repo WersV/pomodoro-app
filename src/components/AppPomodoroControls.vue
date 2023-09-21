ef4379ec382dc5eafa089b862c398ddba151c106
Przemyslaw Botwina <przemek.botwina@o2.pl>
2023-09-22 00:52:18 +0200

    add btns template

---
+++ src/components/AppPomodoroControls.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="pomodoro-controls-container"></div>
+  <button>Pomodoro</button>
+  <button>Short Break</button>
+  <button>Long Break</button>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
