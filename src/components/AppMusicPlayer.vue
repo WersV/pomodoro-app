056b0947873d460069a68be5d2d728c2db4b7623
Przemyslaw Botwina <przemek.botwina@o2.pl>
2023-10-23 23:01:45 +0200

    add spotify player + dissapearing when res is low

--- src/components/AppMusicPlayer.vue
+++ src/components/AppMusicPlayer.vue
@@ -1,31 +1,28 @@
 <template>
-  <div class="song-player">
-    <div class="song-details">
-      <h2 class="song-title"><span>Tiger Tales (of the concrete jungle)</span></h2>
-      <span class="song-author">Tiperpalm*Bay asd as as</span>
-    </div>
-    <div class="song-controls">
-      <div class="song-progress-wrapper">   
-        <div class="song-timer-current">1:23</div>
-        <div class="song-progress-bar-wrapper">
-          <div class="song-progress-bar">
-            <div class="song-progress-fill"></div>
-          </div>
-        </div>
-        <div class="song-timer-max">3:47</div>   
-      </div>
-      <div class="song-control-btns">
-        <button class="song-previous"> <font-awesome-icon icon="fa-solid fa-backward-step"/></button>
-        <button class="song-start-stop"><font-awesome-icon icon="fa-solid fa-play" /></button> 
-        <!-- <font-awesome-icon icon="fa-solid fa-pause" /> -->
-        <button class="song-next"><font-awesome-icon icon="fa-solid fa-forward-step"/></button>
-      </div>
-      <!-- <div class="song-volume"></div> -->
+  <div v-if="width > 750" class="song-player">
+    <div class="spotify-embeed">
+      <iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/37i9dQZF1DWWQRwui0ExPn?utm_source=generator" width="100%" height="100%" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
     </div>
   </div>
 </template>
 
 <script setup>
+import {ref, onMounted, onUnmounted} from 'vue'
+import _ from 'lodash'
+const width = ref(null)
+width.value = window.innerWidth
+console.log(width.value > 750);
+onMounted(() => {
+  window.addEventListener('resize', _.throttle(getWidth,500))
+})
+onUnmounted(() => {
+  window.removeEventListener('resize', getWidth)
+})
+
+function getWidth() {
+   width.value = window.innerWidth
+   console.log('dziala');
+}
 
 </script>
 
@@ -33,81 +30,23 @@
 .song-player {
   box-sizing: border-box;
   position: absolute;
-  bottom: 10%;
-  right: 10%;
-  width: 300px;
-  height: 110px;
-  background-color: rgba(255, 255, 255, .15);
-  backdrop-filter: blur(3px); 
-  padding: 10px;
-  border-radius: 15px;
-  .song-details {
-    display: flex;
-    flex-wrap: wrap;
-    color: white;
-    .song-title {
-      margin: 0;
-      font-size: 16px;
-      overflow: hidden;
-      white-space: nowrap
-    }
-    .song-author {
-      margin-top: 5px;
-      font-size: 13px;
-      width: 90px;
-      overflow: hidden;
-      white-space: nowrap
-    }
+  bottom: 5%;
+  right: 5%;
+  .spotify-embeed {
+    width: 300px;
+    height: 80px;
   }
-  .song-controls {
-    color: white;
-    .song-progress-wrapper {
-      display: flex;
-      justify-content: center;
-      margin-top: 5px;
-      .song-progress-bar-wrapper {
-        position: relative;
-        width: 200px;
-        margin: 0 5px;
-        .song-progress-bar {
-          position: absolute;
-          top: 50%;
-          transform: translateY(-45%);
-          height: 6px;
-          width: 200px;
-          border-radius: 3px;
-          background-color: #919191;
-          overflow: hidden;
-          cursor: pointer;
-          .song-progress-fill {
-            transform: translateX(-50%);
-            width: 200px;
-            height: 6px;
-            border-radius: 3px;
-            background-color: white;
-          }
-        }
-        width: 200px;
-      }
-      .song-timer-current, .song-timer-max {
-        font-size: 12px;
-      }
-    }
+}
 
-    .song-control-btns {
-      display: flex;
-      justify-content: center;
-      margin-top: 5px;
-      .song-previous, .song-start-stop, .song-next {
-        background: transparent;
-        border: 0;
-        color: white;
-        font-size: 25px;
-        cursor: pointer;
-      }
-      .song-start-stop {
-        padding: 0 20px 0 22px;
-      }
+@media(min-width: 750px) {
+  .song-player {
+    box-sizing: border-box;
+    position: absolute;
+    bottom: 10%;
+    right: 10%;
+    .spotify-embeed {
+      width: 350px;
+      height: 160px;
     }
   }
 }
--- src/stores/store.js
+++ src/stores/store.js
@@ -8,16 +8,16 @@ export const useModalStore = defineStore('modal', () => {
   const storeTime = useTimeStore()
 
   const activeTheme = useStorage('activeTheme', 'theme 2')
-  const currentSong = useStorage('currentSong', 'song 1')
-  const songVolume = useStorage('songVolume', '5')
-  const playlist = useStorage('playlist', {})
+  // const currentSong = useStorage('currentSong', 'song 1')
+  // const songVolume = useStorage('songVolume', '5')
+  // const playlist = useStorage('playlist', {})
 
   const isModalDisplayed = ref(false)
 
-  const CLIENT_ID = 'a4289f5a26d04a0b9949ef4d02a2e74c'
-  const CLIENT_SECRET = '1c76667ca7d7441cb893d2194f1a857e'
-  const LOFI_PLAYLIST_LINK = 'https://api.spotify.com/v1/playlists/37i9dQZF1DWWQRwui0ExPn?si=32720bcc3be24004'
-  let ACCESS_TOKEN
+  // const CLIENT_ID = 'a4289f5a26d04a0b9949ef4d02a2e74c'
+  // const CLIENT_SECRET = '1c76667ca7d7441cb893d2194f1a857e'
+  // const LOFI_PLAYLIST_LINK = 'https://api.spotify.com/v1/playlists/37i9dQZF1DWWQRwui0ExPn?si=32720bcc3be24004'
+  // let ACCESS_TOKEN
 
   const bcgThemePaths = [
    'src/assets/img/bcg1.jpg',
@@ -63,34 +63,33 @@ export const useModalStore = defineStore('modal', () => {
     document.body.style.backgroundImage=`url(${bcgThemePaths[index]})`
   }
 
-  async function changeSong() {
-    const response = await fetch("https://accounts.spotify.com/api/token",{
-      method: 'POST',
-      credentials: 'include',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-      },
-      body: `grant_type=client_credentials&client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}`,
-    })
-    const data = await response.json()
-    ACCESS_TOKEN = data.access_token
-    getArtist()
-  }
+  // async function changeSong() {
+  //   const response = await fetch("https://accounts.spotify.com/api/token",{
+  //     method: 'POST',
+  //     credentials: 'include',
+  //     headers: {
+  //       'Content-Type': 'application/x-www-form-urlencoded',
+  //     },
+  //     body: `grant_type=client_credentials&client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}`,
+  //   })
+  //   const data = await response.json()
+  //   ACCESS_TOKEN = data.access_token
+  //   getArtist()
+  // }
 
-  async function getArtist() {
-    console.log(ACCESS_TOKEN);
-    const response = await fetch(LOFI_PLAYLIST_LINK, {
-      headers: {
-        Authorization: `Bearer ${ACCESS_TOKEN}`
-      },
-    })
-    const data = await response.json()
-    console.log(data);
-    playlist.value = data
-  }
+  // async function getArtist() {
+  //   console.log(ACCESS_TOKEN);
+  //   const response = await fetch(LOFI_PLAYLIST_LINK, {
+  //     headers: {
+  //       Authorization: `Bearer ${ACCESS_TOKEN}`
+  //     },
+  //   })
+  //   const data = await response.json()
+  //   console.log(data);
+  //   playlist.value = data
+  // }
 
   changeAppTheme()
-  console.log(playlist.value);
 
-  return { isModalDisplayed, activeTheme, songVolume, $reset, switchModal, saveModalChanges, changeAppTheme }
+  return { isModalDisplayed, activeTheme, $reset, switchModal, saveModalChanges, changeAppTheme }
 }) 
