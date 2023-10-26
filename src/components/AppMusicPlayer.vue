f9260759098ea120db739ff79e2249ef5eeecd17
Przemyslaw Botwina <przemek.botwina@o2.pl>
2023-10-26 08:47:13 +0200

    change player bcg

--- src/components/AppMusicPlayer.vue
+++ src/components/AppMusicPlayer.vue
@@ -1,7 +1,7 @@
 <template>
   <div v-show="width > 750" class="song-player">
     <div class="spotify-embeed">
-      <iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/37i9dQZF1DWWQRwui0ExPn?utm_source=generator&theme=0" width="100%" height="100%" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+      <iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/37i9dQZF1DWWQRwui0ExPn?utm_source=generator" width="100%" height="100%" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
     </div>
   </div>
 </template>
